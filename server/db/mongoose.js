985ea22aef5c00c21462b4a716ebd983e21f2f75
Caique Cunha Pereira <caique_cp@hotmail.com>
2019-01-22 08:16:31 -0200

    add first endpoint: POST todos

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+// set built-in promises as the promise library used by mongoose
+mongoose.Promise = global.Promise
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+
+module.exports = {
+    mongoose
+}
--- server/server.js
+++ server/server.js
@@ -1,70 +1,31 @@
 'use strict'
 
-const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
 
-// set built-in promises as the promise library used by mongoose
-mongoose.Promise = global.Promise
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
 
-// connect to mongodb
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+// environment variables
+const port = process.env.PORT || 3000
 
-// set Todo model schema
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-})
+const app = express()
 
-// set User model schema
-const User = mongoose.model('User', {
-    email: {
-        type: String,
-        require: true,
-        minlength: 1,
-        trim: true
-    }
-})
+// set body-parser as middleware for json
+app.use(bodyParser.json())
 
-// const newTodo = new Todo({
-//     text: 'Cook dinner'
-// })
-
-// newTodo.save()
-//     .then(function (doc) {
-//         console.log('Todo has been saved', doc)
-//     }).catch(function (err) {
-//         console.err(err)
-//     })
-
-// const oneMoreTodo = new Todo({
-//     text: 'Commit changes'
-// })
-
-// oneMoreTodo.save()
-//     .then(function (doc) {
-//         console.log('Todo has been saved', doc)
-//     }).catch(function (err) {
-//         console.err(err)
-//     })
-
-const newUser = new User({
-    email: 'caique@email.com'
-})
-
-newUser.save()
-    .then(function (doc) {
-        console.log('User has been saved', doc)
-    }).catch(function (err) {
-        console.err(err)
+app.post('/todos', function (req, res) {
+    const todo = new Todo({
+        text: req.body.text
     })
+    todo.save().then(function (doc) {
+        res.send(doc)
+    }).catch(function (err) {
+        res.status(500).send(err)
+    })
+})
+
+app.listen(port, function () {
+    console.log(`Server is running on port ${port}`)
+})
