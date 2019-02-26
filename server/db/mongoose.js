ab6eb51b223f6febfac604eb679b56a027ca1741
Caique C Pereira <caique_cp@hotmail.com>
2019-02-26 14:23:35 -0300

    add env variable to set mongodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose')
 // set built-in promises as the promise library used by mongoose
 mongoose.Promise = global.Promise
 
+const mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+
 // connect to mongodb
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
+mongoose.connect(mongoDBURI, { useNewUrlParser: true })
 
 module.exports = {
   mongoose
